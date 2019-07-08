527fc2f5601c2a1d58fbc8ad4721f383d61e4ca1
Biboswan Roy <biboswan98@gmail.com>
2019-07-08 22:26:36 +0530

    Implement censor All or censor selective

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ loadImage = () => {
   const fReader = new FileReader();
   fReader.readAsDataURL(input.files[0]);
   fReader.onloadend = event => {
+    // Cleaning existing censor buttons
+    document.querySelectorAll(".censor-buttton").forEach(el => el.remove());
+
     const source_img = document.querySelector("#source");
     source_img.src = event.target.result;
   };
@@ -28,16 +31,6 @@ loadImage = () => {
 
 addButtonToFaces = outputs => {
   const source_img = document.querySelector("#source");
-
-  //Event delegating for censor face buttons
-  const source_container = document.querySelector("#source-container");
-  source_container.addEventListener("click", e => {
-    console.log(e.target.id);
-    if (e.target.tagName == "BUTTON") {
-      censorFace(i);
-    }
-  });
-
   const sourceWidth = source_img.width;
   const sourceHeight = source_img.height;
   const sourceNaturalWidth = source_img.naturalWidth;
@@ -63,7 +56,7 @@ addButtonToFaces = outputs => {
     btn.style.left = censorX + "px";
     btn.style.top = censorY + "px";
     btn.style.width = boxWidth * displayToNaturalWidthR + "px";
-    btn.innerText = "censor me! ";
+    btn.innerHTML = "censor me! ";
     document.querySelector("#source-container").append(btn);
   }
 };
@@ -71,79 +64,99 @@ addButtonToFaces = outputs => {
 // Censor the detected faces with most appropriate emoji
 onSubmit = async () => {
   const model = await stackml.faceExpression(callbackLoad);
-  const source_img = document.querySelector("#source");
-  const res_img = document.querySelector("#result");
-
   function callbackLoad() {
     console.log("callback after face landmark detection model is loaded!");
   }
 
   // make prediction with the image
+  const source_img = document.querySelector("#source");
   model.detect(source_img, callbackPredict);
 
   // callback after prediction
-  function callbackPredict(err, results) {
-    console.log(results);
+};
 
-    addButtonToFaces(results.outputs);
-    // draw output keypoints in the image
+callbackPredict = (err, results) => {
+  const source_img = document.querySelector("#source");
+  const res_img = document.querySelector("#result");
+  const censorAllBtn = document.querySelector("#censor-all-btn");
+  censorAllBtn.disabled = true;
 
-    const sourceWidth = source_img.width;
-    const sourceHeight = source_img.height;
+  console.log(results);
 
-    res_img.width = sourceWidth;
-    res_img.height = sourceHeight;
-    res_img.style.height = sourceHeight + "px";
-    res_img.style.width = sourceWidth + "px";
-
-    const sourceNaturalWidth = source_img.naturalWidth;
-    const sourceNaturalHeight = source_img.naturalHeight;
-
-    const displayToNaturalWidthR = sourceWidth / sourceNaturalWidth;
-    const displayToNaturalHeightR = sourceHeight / sourceNaturalHeight;
-
-    const ctx = res_img.getContext("2d");
-
-    ctx.drawImage(
-      source_img,
-      0,
-      0,
-      sourceNaturalWidth,
-      sourceNaturalHeight,
-      0,
-      0,
-      sourceWidth,
-      sourceHeight
-    );
-    ctx.textAlign = "center";
-
-    const { outputs } = results;
-    const outputLen = outputs.length;
-
-    let maxP;
-    for (let i = 0; i < outputLen; i++) {
-      const output = outputs[i];
-      maxP = 0;
-      //Find most suitable expression i.e having highest probability
-      for (let j = 0; j < 7; j++) {
-        if (maxP < output.expressions[j].probability) {
-          emoji = output.expressions[j].expression;
-          maxP = output.expressions[j].probability;
-        }
-      }
-
-      const boxWidth = output.detection.box.width;
-      const boxHeight = output.detection.box.height;
-
-      const censorX =
-        (output.detection.box.x + boxWidth / 2) * displayToNaturalWidthR;
-      const censorY =
-        (output.detection.box.y + boxHeight) * displayToNaturalHeightR;
-
-      ctx.font = `${boxHeight * displayToNaturalHeightR}px Arial`;
-      ctx.fillText(express[emoji], censorX, censorY);
+  //Event delegating for censor face buttons
+  const source_container = document.querySelector("#source-container");
+  source_container.addEventListener("click", e => {
+    console.log(e.target.id);
+    if (e.target.tagName == "BUTTON") {
+      censorFace(e.target.id);
     }
-  }
+  });
+
+  const { outputs } = results;
+  const outputLen = outputs.length;
+
+  censorAllBtn.disabled = false;
+  censorAllBtn.addEventListener("click", () => {
+    for (let i = 0; i < outputLen; i++) {
+      censorFace(i);
+    }
+  });
+
+  addButtonToFaces(results.outputs);
+
+  // draw output keypoints in the image
+
+  const sourceWidth = source_img.width;
+  const sourceHeight = source_img.height;
+
+  res_img.width = sourceWidth;
+  res_img.height = sourceHeight;
+  res_img.style.height = sourceHeight + "px";
+  res_img.style.width = sourceWidth + "px";
+
+  const sourceNaturalWidth = source_img.naturalWidth;
+  const sourceNaturalHeight = source_img.naturalHeight;
+
+  const displayToNaturalWidthR = sourceWidth / sourceNaturalWidth;
+  const displayToNaturalHeightR = sourceHeight / sourceNaturalHeight;
+
+  const ctx = res_img.getContext("2d");
+
+  ctx.drawImage(
+    source_img,
+    0,
+    0,
+    sourceNaturalWidth,
+    sourceNaturalHeight,
+    0,
+    0,
+    sourceWidth,
+    sourceHeight
+  );
+  ctx.textAlign = "center";
+
+  const censorFace = i => {
+    const output = outputs[i];
+    let maxP = 0;
+    //Find most suitable expression i.e having highest probability
+    for (let j = 0; j < 7; j++) {
+      if (maxP < output.expressions[j].probability) {
+        emoji = output.expressions[j].expression;
+        maxP = output.expressions[j].probability;
+      }
+    }
+
+    const boxWidth = output.detection.box.width;
+    const boxHeight = output.detection.box.height;
+
+    const censorX =
+      (output.detection.box.x + boxWidth / 2) * displayToNaturalWidthR;
+    const censorY =
+      (output.detection.box.y + boxHeight) * displayToNaturalHeightR;
+
+    ctx.font = `${boxHeight * displayToNaturalHeightR}px Arial`;
+    ctx.fillText(express[emoji], censorX, censorY);
+  };
 };
 
 init();
